20d35016066c5cab89538ac92f23eb818ea517c4
marcel <m.f.zelazny@student.tue.nl>
2025-06-10 01:38:01 +0200

    Update init_data.py

--- Dashboard/init_data.py
+++ Dashboard/init_data.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 df = pd.read_csv("Data/Data - Transformed.csv", parse_dates=['Date'])
 df.dropna()
-df.drop(columns=['Unnamed: 0', 'LSOA name', 'Ward code', 'Ward name'], inplace=True)
+df.drop(columns=['Unnamed: 0', 'Ward code', 'Ward name'], inplace=True)
 df['YearMonth'] = df['Date'].dt.to_period('M')
 
-df = df.groupby(['YearMonth', 'LSOA code']).size().reset_index()
+df = df.groupby(['YearMonth', 'LSOA code', 'LSOA name']).size().reset_index()
 df = df.rename({0: "Burglaries"}, axis=1)
 df['Year'] = df['YearMonth'].dt.year
 df['Month'] = df['YearMonth'].dt.month
